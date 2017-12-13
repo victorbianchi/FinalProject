b3a13d45f8d925add7356eed61f750993fcfb4e4
subeen-kim <subeen.kim@students.olin.edu>
2017-12-13 04:03:25 -0500

    body is well constructed, needs time as a variable

--- bipedal.py
+++ bipedal.py
@@ -1,10 +1,12 @@
 import Box2D
 import random
+from math import cos, sin, pi
 from Box2D.b2 import (world, circleShape, staticBody, dynamicBody, polygonShape)
 
 MAX_TORQUE = 1e4
 WHEEL_FRICTION = 5.0
-SPEED = 5
+#CHANGED SPEED TO NEGATIVE TO MOVE FORWARD
+SPEED = -5
 
 class Bipedal:
 
@@ -19,39 +21,40 @@ class Bipedal:
         length2 = self.genome['left_leg']
 
         body = world.CreateDynamicBody(position=(x0, y0))
+        body.fixedRotation = True
         body.CreatePolygonFixture(box=(width, height), density=1, friction=0.3)
 
         head = world.CreateDynamicBody(position=(x0, y0+4))
+        head.fixedRotation = True
         head.CreateCircleFixture(radius = 1.8, density=1, friction=0.3)
 
         neck = world.CreateDynamicBody(position=(x0, y0+2.5))
+        neck.fixedRotation = True
         neck.CreatePolygonFixture(box = (0.5, 1), density=1, friction=WHEEL_FRICTION)
 
+        #world.CreateDistanceJoint(bodyA = head, bodyB = body, anchorA = head.worldCenter, anchorB= body.worldCenter,
+        #collideConnected = True)
         world.CreateRevoluteJoint(bodyA=head, bodyB=neck, anchor = head.worldCenter, enableMotor = False)
         world.CreateRevoluteJoint(bodyA=neck, bodyB=body, anchor = body.worldCenter, enableMotor = False)
 
-        right_leg = world.CreateDynamicBody(position=(x0+0.2, y0-2))
+        right_leg = world.CreateDynamicBody(position=(x0+1, y0-2))
         right_leg.CreatePolygonFixture(box = (0.5,3), density=1, friction=WHEEL_FRICTION)
-        world.CreateRevoluteJoint(bodyA=right_leg, bodyB=body,
-                anchor = body.worldCenter,
-                lowerAngle = -0.5 * 3.14, upperAngle = 0.5 * 3.14, enableLimit = True,
-                maxMotorTorque = MAX_TORQUE, motorSpeed = SPEED,
+        world.CreateRevoluteJoint(bodyA=body, bodyB=right_leg,
+                anchor = body.worldCenter + (1,-2),
+                lowerAngle = -0.3 * pi, upperAngle = 0.3 * pi,
+                enableLimit = True,
+                maxMotorTorque = MAX_TORQUE, motorSpeed = cos(SPEED),
                 enableMotor = True)
 
-        left_leg = world.CreateDynamicBody(position=(x0-0.2, y0-2))
+        left_leg = world.CreateDynamicBody(position=(x0-1, y0-2))
         left_leg.CreatePolygonFixture(box = (0.5,3), density=1, friction=WHEEL_FRICTION)
-        world.CreateRevoluteJoint(bodyA=left_leg, bodyB=right_leg,
-                anchor = body.worldCenter,
-                lowerAngle = -0.5 * 3.14, upperAngle = 0.5 * 3.14,
+        world.CreateRevoluteJoint(bodyA=body, bodyB=left_leg,
+                anchor = body.worldCenter + (-1, -2),
+                lowerAngle = -0.3 * pi, upperAngle = 0.3 * pi,
                 enableLimit = True,
-                maxMotorTorque = MAX_TORQUE, motorSpeed = SPEED,
+                maxMotorTorque = MAX_TORQUE, motorSpeed = sin(SPEED),
                 enableMotor = True)
-        #left_leg = world.CreateDynamicBody(position=(x0+width*0.95, y0-height*0.85))
-        #right_leg.CreatePolygonFixture(box= (3,6), density=1, friction=WHEEL_FRICTION)
-        #world.CreateRevoluteJoint(bodyA=body, bodyB=left_leg,
-        #        anchor = left_leg.worldCenter,
-        #        maxMotorTorque = MAX_TORQUE, motorSpeed = SPEED,
-        #        enableMotor = True)
+
 
         self.bodies = [body, head, neck, right_leg, left_leg]
         self.tracker = body.worldCenter
