ed52a7b4dfa2ae47a59e7ca0a0b69754bc401788
subeen-kim <subeen.kim@students.olin.edu>
2017-12-13 04:15:41 -0500

    I made proper documentation in the bipedal.py

--- bipedal.py
+++ bipedal.py
@@ -1,18 +1,25 @@
+"""
+bipedal.py constructs bipedal creatures based on Box2D module
+it gets width and height from the sim.py
+and returns the bipedal objects.
+"""
 import Box2D
 import random
 from math import cos, sin, pi
 from Box2D.b2 import (world, circleShape, staticBody, dynamicBody, polygonShape)
 
+"""define variables"""
 MAX_TORQUE = 1e4
 WHEEL_FRICTION = 5.0
 #CHANGED SPEED TO NEGATIVE TO MOVE FORWARD
-SPEED = -5
+SPEED = 5
 
 class Bipedal:
-
+    """ designate the physical variable with the genes """
     def __init__(self, name, gene):
         self.genome = {'width': gene[0],'height': gene[1],'right_leg': gene[2],'left_leg': gene[2]}
         self.name = name
+
 
     def build(self, world, x0, y0):
         width = self.genome['width']
@@ -20,6 +27,7 @@ class Bipedal:
         length1 = self.genome['right_leg']
         length2 = self.genome['left_leg']
 
+        """ construct body, head, and neck """
         body = world.CreateDynamicBody(position=(x0, y0))
         body.fixedRotation = True
         body.CreatePolygonFixture(box=(width, height), density=1, friction=0.3)
@@ -32,11 +40,11 @@ class Bipedal:
         neck.fixedRotation = True
         neck.CreatePolygonFixture(box = (0.5, 1), density=1, friction=WHEEL_FRICTION)
 
-        #world.CreateDistanceJoint(bodyA = head, bodyB = body, anchorA = head.worldCenter, anchorB= body.worldCenter,
-        #collideConnected = True)
+        """ Connect the body with the joints, but rotation is not allowed. """
         world.CreateRevoluteJoint(bodyA=head, bodyB=neck, anchor = head.worldCenter, enableMotor = False)
         world.CreateRevoluteJoint(bodyA=neck, bodyB=body, anchor = body.worldCenter, enableMotor = False)
 
+        """ Construct the leg and connect it with the body """
         right_leg = world.CreateDynamicBody(position=(x0+1, y0-2))
         right_leg.CreatePolygonFixture(box = (0.5,3), density=1, friction=WHEEL_FRICTION)
         world.CreateRevoluteJoint(bodyA=body, bodyB=right_leg,
@@ -55,6 +63,7 @@ class Bipedal:
                 maxMotorTorque = MAX_TORQUE, motorSpeed = sin(SPEED),
                 enableMotor = True)
 
+        print(width, height)
 
         self.bodies = [body, head, neck, right_leg, left_leg]
         self.tracker = body.worldCenter
